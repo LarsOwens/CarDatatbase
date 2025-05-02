6a18dd140d437685a89df934b4969e713a5e91b3
LarsOwens <104724741+LarsOwens@users.noreply.github.com>
2025-05-02 10:11:19 -0400

    Update car_data_project.py

--- car_data_project.py
+++ car_data_project.py
@@ -1,5 +1,5 @@
 # ## First attempt
-# ## Worls but doesn't store user entry beyond the sesion, lines 3-30
+# ## Works but doesn't store user entry beyond the sesion, lines 3-30
 # from dataclasses import dataclass
 
 # @dataclass
